9033e7a113e59328d7d94a9c6bcc4641b8c44b1f
andrey05061975 <79269588653@yandex.ru>
2022-04-10 17:37:15 +0300

    lesson-1

---
+++ src/main/java/ru/geekbrains/lesson1/FirstApp.java
@@ -0,0 +1,55 @@
+package ru.geekbrains.lesson1;
+
+public class FirstApp {
+    public static void main(String[] args) {
+        FirstApp.printThreeWords();
+        FirstApp.checkSumSign();
+        FirstApp.printColor();
+        FirstApp.compareNumbers();
+
+
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 30;
+        int b = 20;
+        int c = a + b;
+
+        if (c > 0) {
+            System.out.println("сумма положительная");
+        } else {
+            System.out.println("сумма отрицательная");
+        }
+
+        System.out.println("c=" + c);
+    }
+
+    public static void printColor() {
+        int value = -250;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        else if (value >= 1 && value <= 100) {
+            System.out.println("Желтый");
+        }
+       else{
+            System.out.println("Зеленый");
+       }
+    }
+    public static void compareNumbers() {
+        int a = 30;
+        int b = 50;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else if (a < b) {
+            System.out.println("a < b");
+        }
+    }
+}
